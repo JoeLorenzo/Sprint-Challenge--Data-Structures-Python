62c5ece63bbfd4cd764a6e78b0cbb6fcdcd1495f
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-07-08 18:44:02 -0500

    -finished names.py

--- names/names.py
+++ names/names.py
@@ -1,4 +1,31 @@
+from bst import BSTNode
 import time
+
+
+# start_time = time.time()
+
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# duplicates = []  # Return the list of duplicates in this data structure
+
+# # because this is a nested loop, the time complexity of this code is O(n^2)
+
+# # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# end_time = time.time()
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime: {end_time - start_time} seconds")
+
 
 start_time = time.time()
 
@@ -10,21 +37,56 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+duplicates2 = []  # Return the list of duplicates in this data structure
 
-# because this is a nested loop, the time complexity of this code is O(n^2)  (O of n squared)
+binary = BSTNode("")
 
-# Replace the nested for loops below with your improvements
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    binary.insert(name_1)
+
+for name_2 in names_2:
+    if binary.contains(name_2): 
+        duplicates2.append(name_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"{len(duplicates2)} duplicates:\n\n{', '.join(duplicates2)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+
+end_time = time.time()
+print (f"{len(duplicates2)} duplicates:\n\n{', '.join(duplicates2)}\n\n")
+print (f"runtime2: {end_time - start_time} seconds")
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+names = []
+duplicates3 = [] # Return the list of duplicates in this data structure
+for name_1 in names_1:
+    names.append(name_1)
+
+uniques = set(names)
+
+for name_2 in names_2:
+    if name_2 in uniques:
+        duplicates3.append(name_2)
+
+
+end_time = time.time()
+print (f"{len(duplicates3)} duplicates:\n\n{', '.join(duplicates3)}\n\n")
+print (f"runtime3: {end_time - start_time} seconds")
+
+print(len(duplicates3))

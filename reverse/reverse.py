096c94ebbb6ec6898eb92278e1ebd234203d6d74
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-07-08 20:00:06 -0500

    -finished reverse.py

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -38,5 +38,60 @@ class LinkedList:
 
         return False
 
+    # utility function to print elements
+    def print_list(self):
+        arr = []
+        if not self.head:
+            return arr
+
+        current = self.head
+
+        while current:
+            if current.get_next() == None:
+                arr.append(current.get_value())
+                return arr
+
+            else:
+                arr.append(current.get_value())
+                current = current.get_next()
+
+        return arr
+
+        
+
     def reverse_list(self, node, prev):
-        pass
+        # intialize a variable to capture the previous node
+        prev = None
+        # intialize a variable to capture the current node
+        current = self.head 
+        while current is not None: 
+            # initialize a variable to capture the next node
+            next_node = current.get_next()
+            # reversing starts here
+            # set the current next node to the previous node
+            current.set_next(prev)
+            # the previous node becomes the current node
+            prev = current
+            # head becomes previous node 
+            self.head = prev 
+            # the current node becomes the next node
+            current = next_node
+        
+        # print(self.print_list())
+
+
+
+
+test_list = LinkedList() 
+test_list.add_to_head(1)
+test_list.add_to_head(2)
+test_list.add_to_head(3)
+test_list.add_to_head(4)
+test_list.add_to_head(5)
+test_list.add_to_head(6)
+
+print(test_list.head.value)
+print(test_list.print_list())
+
+print(test_list.reverse_list(None, None))
+print(test_list.head.value)
